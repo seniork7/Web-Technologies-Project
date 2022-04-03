da5179a0e03a8f31f64e2f32a8fa1582a027036e
seniork7 <seniorstudent81@gmail.com>
2022-04-03 11:33:29 -0500

    seventh commit

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
-$(function(){
-    $("card-img-top").click(function(){
-      $("#div1").fadeIn();
-      $("#div2").fadeIn("slow");
-      $("#div3").fadeIn(3000);
-    });
-  });
+// //var name = 'Senior';
+// var num = 50;
+// var sum = name + num + 30;
+// //console.log(sum);
+// var Fname = prompt('What is your first name?')
+// var Lname = prompt('What is your last name?')
+// var name = Fname + ' ' + Lname
+// var num1 = 10
+// var num2 = 50
+// var total = Number(num2) + Number(10)
+// console.log(total)
+
+// let grade = 90
+
+// if(grade === 80)
+// {
+//     alert("Pass")
+// }
+// else
+// {
+//    alert ("Fail")
+// }
+
+// var na ='kevon';
+// // console.log(na.toUpperCase());
+// // console.log(na.toLowerCase());
+
+// console.log(na.substring(0, 4))
+
+
+// for(i = 1; i <= 5; i++)
+// {
+//     console.log(i)
+// }
+
+// const name = ['Kevon', 'Andre', 'Senior']
+
+// // for(i = 0; i < 2; i++)
+// // {
+// //     console.log(name[1])
+// // }
+
+// const bar = name.map(function(barName) {
+//     return barName
+// })
+// console.log(bar)
+
+// function myFunction() 
+// {
+//     console.log('Hello, my name is Kevon!', 
+//              'I am an aspiring developer')
+// }
+
+// myFunction()
+
+// const dam = (document.getElementsByClassName('col'));
+
+// console.log(dam[4])
