c5ba50489d8bba05a3b28a315db467b93ab95289
seniork7 <seniorstudent81@gmail.com>
2022-05-08 16:51:56 -0500

    9th commit

--- script.js
+++ script.js
@@ -57,3 +57,18 @@
 
 // console.log(dam[4])
 
+// $(document).ready(function(){
+//     $("p").fadeIn(5000);
+// })
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
